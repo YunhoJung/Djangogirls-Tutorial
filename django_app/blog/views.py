cfb8fee675b1c7291df17d5d3c810878c476ba5d
Jung YunHo <saijdu2198@gmail.com>
2017-05-30 11:30:46 -0700

    set STATIC_DIR to STATICFILES_DIRS, use static tag at post_list.html

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -8,7 +8,7 @@ def post_list(request):
     # posts 변수에 ORM을 이용해서 전체 Post의 리스트(쿼리셋)를 대입
     # posts변수에 ORM을 이용해서 published_date timezone을 이용해서
     # 최근 이전에 퍼블리쉬 되었던 글들만 전달
-    posts = Post.objects.all().filter(published_date__lte=timezone.now())
+    posts = Post.objects.filter(published_date__lte=timezone.now())
     context = {
         'title': 'PostList from post_list view',
         'posts': posts
